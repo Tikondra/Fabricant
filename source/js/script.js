f77f2529617b2420e09e4ae92fc7e934be6c8e73
Tikondra <tikondra47@gmail.com>
2020-02-09 21:25:06 +0400

    slider

--- source/js/script.js
+++ source/js/script.js
@@ -18,3 +18,37 @@ $(document).ready(function(){
     nav: true
   });
 });
+// слайдер
+(function () {
+  var slider = document.querySelector('.slider');
+  var sliderList = slider.querySelector('.slider__list');
+  var slides = slider.querySelectorAll('.slider__item');
+  var btnPrev = slider.querySelector('.slider__btn-prev');
+  var btnNext = slider.querySelector('.slider__btn-next');
+  var pagNumber = slider.querySelector('.slider__active-pag');
+  var countSlides = slides.length;
+  var gap = 20;
+
+  var onMoveSlideNext = function () {
+    var widthList = sliderList.getBoundingClientRect().width;
+    var right = sliderList.getBoundingClientRect().left - gap;
+    var maxLeft = - ((countSlides * widthList) - widthList);
+    var newLeft = right - widthList;
+    if (newLeft < maxLeft) {
+      newLeft = maxLeft;
+    }
+    return sliderList.style.left = newLeft + 'px';
+  };
+  var onMoveSlidePrev = function () {
+    var widthList = sliderList.getBoundingClientRect().width;
+    var right = sliderList.getBoundingClientRect().left - gap;
+    var minLeft = 0;
+    var newLeft = right + widthList;
+    if (newLeft > minLeft) {
+      newLeft = minLeft;
+    }
+    return sliderList.style.left = newLeft + 'px';
+  };
+  btnNext.addEventListener('click', onMoveSlideNext);
+  btnPrev.addEventListener('click', onMoveSlidePrev);
+})();
