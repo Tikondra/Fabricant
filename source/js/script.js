e35a145daaa86a2242fc84b838306eda2fb12f13
Tikondra <tikondra47@gmail.com>
2020-02-10 16:13:49 +0400

    slider2

--- source/js/script.js
+++ source/js/script.js
@@ -12,53 +12,71 @@
 })();
 // слайдер
 (function () {
-  var slider = document.querySelector('.slider');
-  var sliderList = slider.querySelector('.slider__list');
-  var slides = slider.querySelectorAll('.slider__item');
-  var btnPrev = slider.querySelector('.slider__btn--prev');
-  var btnNext = slider.querySelector('.slider__btn--next');
-  var pagNumber = slider.querySelector('.slider__active-pag');
-  var pagAllCount = slider.querySelector('.slider__all-pag');
-  var countSlides = slides.length;
-  var gap = 20;
+  var sliderBanner = document.querySelector('.slider');
+  var sliderPortfolio = document.querySelector('.portfolio__slider');
 
-  pagAllCount.textContent = '0' + slides.length;
+  var sliderInit = function (slider) {
+    var sliderList = slider.querySelector('.slider__list');
+    var slides = slider.querySelectorAll('.slider__item');
+    var btnPrev = slider.querySelector('.slider__btn--prev');
+    var btnNext = slider.querySelector('.slider__btn--next');
+    var pagNumber = slider.querySelector('.slider__active-pag');
+    var pagAllCount = slider.querySelector('.slider__all-pag');
+    var countSlides = slides.length;
 
-  var onMoveSlideNext = function () {
-    var widthList = sliderList.getBoundingClientRect().width;
-    var right = (getComputedStyle(sliderList).left).slice(0, -2);
-    var maxLeft = - ((countSlides * widthList) - widthList);
-    var pagActive = Number(pagNumber.textContent);
-    var maxPag = countSlides;
-    var newLeft = right - widthList;
+    var onMoveSlideNext = function () {
+      var widthList = sliderList.getBoundingClientRect().width;
+      var right = (getComputedStyle(sliderList).left).slice(0, -2);
+      var maxLeft = - ((countSlides * widthList) - widthList);
+      var pagActive = Number(pagNumber.textContent);
+      var maxPag = countSlides;
+      var newLeft = right - widthList;
 
-    if (newLeft < maxLeft) {
-      newLeft = maxLeft;
+      if (newLeft < maxLeft) {
+        newLeft = maxLeft;
+      }
+      if (pagActive >= maxPag) {
+        pagNumber.textContent = '0' + maxPag;
+      } else {
+        pagNumber.textContent = '0' + (pagActive + 1);
+      }
+      sliderList.style.left = newLeft + 'px';
+    };
+    var onMoveSlidePrev = function () {
+      var widthList = sliderList.getBoundingClientRect().width;
+      var right = Math.floor((getComputedStyle(sliderList).left).slice(0, -2));
+      var pagActive = Number(pagNumber.textContent);
+      var minLeft = 0;
+      var minPag = 1;
+      var newLeft = right + widthList;
+      if (newLeft > minLeft) {
+        newLeft = minLeft;
+      }
+      if (pagActive <= minPag) {
+        pagNumber.textContent = '0' + minPag;
+      } else {
+        pagNumber.textContent = '0' + (pagActive - 1);
+      }
+      return sliderList.style.left = newLeft + 'px';
+    };
+    var showInfo = function () {
+      var info = slider.querySelectorAll('.slider__info');
+      var btnInfo = slider.querySelector('.slider__btn-info');
+
+      btnInfo.addEventListener('click', function () {
+        info.forEach(function (item) {
+          item.classList.toggle('slider__info--show');
+        })
+      })
+    };
+    if (slider.querySelector('.slider__btn-info')) {
+      showInfo();
     }
-    if (pagActive >= maxPag) {
-      pagNumber.textContent = '0' + maxPag;
-    } else {
-      pagNumber.textContent = '0' + (pagActive + 1);
-    }
-    sliderList.style.left = newLeft + 'px';
+
+    pagAllCount.textContent = '0' + slides.length;
+    btnNext.addEventListener('click', onMoveSlideNext);
+    btnPrev.addEventListener('click', onMoveSlidePrev);
   };
-  var onMoveSlidePrev = function () {
-    var widthList = sliderList.getBoundingClientRect().width;
-    var right = sliderList.getBoundingClientRect().left - gap;
-    var pagActive = Number(pagNumber.textContent);
-    var minLeft = 0;
-    var minPag = 1;
-    var newLeft = right + widthList;
-    if (newLeft > minLeft) {
-      newLeft = minLeft;
-    }
-    if (pagActive <= minPag) {
-      pagNumber.textContent = '0' + minPag;
-    } else {
-      pagNumber.textContent = '0' + (pagActive - 1);
-    }
-    return sliderList.style.left = newLeft + 'px';
-  };
-  btnNext.addEventListener('click', onMoveSlideNext);
-  btnPrev.addEventListener('click', onMoveSlidePrev);
+  sliderInit(sliderBanner);
+  sliderInit(sliderPortfolio);
 })();
