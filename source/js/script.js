79a6aa2ef7a1d94df17e77fd1dc78926f566b789
Tikondra <tikondra47@gmail.com>
2020-02-10 10:33:17 +0400

    banner m

--- source/js/script.js
+++ source/js/script.js
@@ -23,29 +23,46 @@ $(document).ready(function(){
   var slider = document.querySelector('.slider');
   var sliderList = slider.querySelector('.slider__list');
   var slides = slider.querySelectorAll('.slider__item');
-  var btnPrev = slider.querySelector('.slider__btn-prev');
-  var btnNext = slider.querySelector('.slider__btn-next');
+  var btnPrev = slider.querySelector('.slider__btn--prev');
+  var btnNext = slider.querySelector('.slider__btn--next');
   var pagNumber = slider.querySelector('.slider__active-pag');
+  var pagAllCount = slider.querySelector('.slider__all-pag');
   var countSlides = slides.length;
   var gap = 20;
+
+  pagAllCount.textContent = '0' + slides.length;
 
   var onMoveSlideNext = function () {
     var widthList = sliderList.getBoundingClientRect().width;
     var right = sliderList.getBoundingClientRect().left - gap;
     var maxLeft = - ((countSlides * widthList) - widthList);
+    var pagActive = Number(pagNumber.textContent);
+    var maxPag = countSlides;
     var newLeft = right - widthList;
     if (newLeft < maxLeft) {
       newLeft = maxLeft;
     }
-    return sliderList.style.left = newLeft + 'px';
+    if (pagActive >= maxPag) {
+      pagNumber.textContent = '0' + maxPag;
+    } else {
+      pagNumber.textContent = '0' + (pagActive + 1);
+    }
+    sliderList.style.left = newLeft + 'px';
   };
   var onMoveSlidePrev = function () {
     var widthList = sliderList.getBoundingClientRect().width;
     var right = sliderList.getBoundingClientRect().left - gap;
+    var pagActive = Number(pagNumber.textContent);
     var minLeft = 0;
+    var minPag = 1;
     var newLeft = right + widthList;
     if (newLeft > minLeft) {
       newLeft = minLeft;
+    }
+    if (pagActive <= minPag) {
+      pagNumber.textContent = '0' + minPag;
+    } else {
+      pagNumber.textContent = '0' + (pagActive - 1);
     }
     return sliderList.style.left = newLeft + 'px';
   };
